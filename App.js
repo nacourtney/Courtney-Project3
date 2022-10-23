e729961f3b5de02d9d9469498e97a0d5ecbda74c
Nichole Courtney <nichole.a.courtney@gmail.com>
2022-10-23 13:32:26 -0600

    Update: API calls working

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Button,
+  ScrollView,
+  Pressable,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const letters = [
+  { letter: "A" },
+  { letter: "B" },
+  { letter: "C" },
+  { letter: "D" },
+  { letter: "E" },
+];
+
+const letterIndex = "";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLetterIndex: 0,
+      results: "",
+    };
+  }
+
+  dynamicLabels = function (props) {
+    return "Students who got a/an " + props + " grade.";
+  };
+
+  onLoad = async () => {
+    this.setState({ results: "Loading, please wait..." });
+    const response = await fetch(
+      "https://2s4b8wlhik.execute-api.us-east-1.amazonaws.com/studentData?grade=" +
+        letters[this.state.selectedLetterIndex].letter,
+      {
+        method: "GET",
+      }
+    );
+
+    const results = await response.text();
+    this.setState({ results });
+  };
+
+  render() {
+    const { results } = this.state;
+    const buttons = letters.map((letter, index) => {
+      return (
+        <Pressable
+          onPressIn={() => this.setState({ selectedLetterIndex: index })}
+          buttonStyle={styles.button}
+          key={letters.letter}
+          onPress={this.onLoad}
+          title={letter.letter}
+        >
+          <Text>{this.dynamicLabels(letter.letter)}</Text>
+          <Text>
+            {letter.letter} ({index})
+          </Text>
+        </Pressable>
+      );
+    });
+
+    return (
+      <View style={styles.container}>
+        <View>
+          <TextInput
+            style={styles.preview}
+            value={results}
+            placeholder="Results..."
+            editable={false}
+            multiline
+          />
+        </View>
+        <View>{buttons}</View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  preview: {
+    backgroundColor: "#bdc3c7",
+    width: 300,
+    height: 400,
+    padding: 10,
+    borderRadius: 5,
+    color: "#333",
+    marginBottom: 50,
+  },
+  button: {
+    margin: 15,
   },
 });
